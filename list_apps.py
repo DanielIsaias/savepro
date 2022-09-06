d8c0df6f87ea5223798d8a0abe92c709de5dab8a
DanielIsaias <danielisaias460@gmail.com>
2022-09-06 10:09:19 -0700

    Mandamos todo a github

--- list_apps.py
+++ list_apps.py
@@ -9,15 +9,25 @@ terminal_programs = []
 
 #The type of installation or the type of repo 
 #this way can avoid use (if and else) 
-type_of_installation = {'-r': "sudo apt install " } 
+type_of_installation = {'-repo': "sudo apt install " } 
 
 
 
 
-def recapp(repo, nameOfApp):
+def record_install(repo, nameOfApp):
     command = type_of_installation[repo] + nameOfApp 
-    os.system(command) 
+    os.system(command) #la mejor practica es obtener la salida del comando para poder manejar los errores que surgan
     terminal_programs.append(nameOfApp) 
+
+
+
+
+#nos muestra una lista de los progrmas instalados 
+def list_terminal_apps():
+    for i in range(len(terminal_programs)):
+        print(str(i) + terminal_programs ) 
+
+
 
 
 
