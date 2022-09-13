47bd41420e6cf81a4f880b8ca2195f94afb48d90
DanielIsaias <danielisaias460@gmail.com>
2022-09-13 12:15:01 -0700

    save the changes to have a backup

--- savepro.py
+++ savepro.py
@@ -1,31 +1,73 @@
-
+import subprocess as sub
 import os
+
+#With this file we store the record of installed Programs
+import list_of_programms
 
 
 
 
 #The list can be replaced by a file to store permanently the programs we have
-terminal_programs = [] 
-
-#The type of installation or the type of repo 
-#this way can avoid use (if and else) 
-type_of_installation = {'-repo': "sudo apt install " } 
-
+repo_programs = [] 
 
 
 
 def record_install(repo, nameOfApp):
-    command = type_of_installation[repo] + nameOfApp 
-    os.system(command) #la mejor practica es obtener la salida del comando para poder manejar los errores que surgan
-    terminal_programs.append(nameOfApp) 
+    #The type of instalation is from where we install the program 
+    #Using dictionaris we can avoid use ifElse 
+    type_of_installation = {'-repo': "sudo apt install "} 
+
+    install = type_of_installation[repo] + nameOfApp 
+
+    os.system(install) 
+    #la mejor practica es obtener la salida del comando para poder manejar los errores que surgan
+    
+
+    #If every thing is ok, we can store the program
+     
+    infoCommand = 'whatis {} | cut -c 24-'.format(nameOfApp) 
+    appInfo = sub.run(infoCommand, capture_output=True, text=True, shell=True) 
+
+
+    repo_programs.append([nameOfApp,appInfo.stdout]) 
+
+
+
+#Nos falta agregar aque lista se aguardara
+#mejor usaremos una funcion para guardar el programa junto con su descripsion
+def add_program(name): 
+    
+    #infoCommand = 
+
+    list_of_programms.repo_programs.append(name)
 
 
 
 
-#nos muestra una lista de los progrmas instalados 
+#Nos muestra una lista de los progrmas instalados 
+#La idea es que nos lo muestre en un tabla de manera ordenada
 def list_terminal_apps():
-    for i in range(len(terminal_programs)):
-        print(str(i) + terminal_programs ) 
+    for i in range(len(repo_programs)):
+        print(repo_programs[i]) 
+
+
+
+answer = input("Enter program name:")
+repo = input("repo:") 
+record_install(repo, answer)
+
+
+list_terminal_apps() 
+
+
+
+
+
+
+
+
+
+
 
 
 
